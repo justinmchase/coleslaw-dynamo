60afe05833624e77e4f812ed5fdea6f4e07d387e
Justin Chase <justin.m.chase@gmail.com>
2016-05-09 09:12:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var assert = require('assert')
+var vogels = require('vogels')
+var Joi = require('joi')
+
+function create(options, models) {
+    assert(options)
+    assert(models)
+    
+    if (!models.length) models = [models]
+    
+    var dataAccess = {}
+    models.forEach(model => {
+        var indexFields = model.fields.filter(f => f.index)
+        if (!indexFields.length) throw new TypeError(`Model '${model.name}' must have at least one id field`)
+        
+        var schema = {}
+        model.fields.forEach(field => {
+            switch(field.type) {
+                case 'string':
+                    return schema[field.name] = Joi.string()
+                case 'number':
+                    return schema[field.name] = Joi.number()
+                case 'boolean':
+                    return schema[field.name] = Joi.boolean()
+                default:
+                    throw new TypeError(`Field type '${field.type}' not yet supported`)
+            }
+        })
+
+        var indexes = []
+        indexFields.forEach(field => {
+            indexes.push({
+                name: `${model.name}${field.name}Index`,
+                hashKey: field.name,
+                type: 'global'
+            })
+        })
+        
+        dataAccess[model.name] = vogels.define(model.name, {
+            hashKey: indexFields[0].name,
+            timestamps: true,
+            schema: schema,
+            indexes: indexes
+        })
+    })
+    
+    return dataAccess
+}
+
+module.exports = {
+    create: create
+}
